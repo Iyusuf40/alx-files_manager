bd48482825e68c293ebab209775290c9ef4a9ea6
Iyusuf40 <punkgts40@gmail.com>
2023-03-03 17:27:19 +0100

    update utils file

--- utils/db.js
+++ utils/db.js
@@ -16,6 +16,8 @@ class DBClient {
     await this.client.connect();
     this.connected = true;
     this.db = this.client.db(dbName);
+    this.filesCollection = this.db.collection('files');
+    this.usersCollection = this.db.collection('users');
   }
 
   isAlive() {
@@ -33,6 +35,28 @@ class DBClient {
     const noOfFiles = await filesCollection.countDocuments({});
     return noOfFiles;
   }
+
+  async findByColAndFilter(collection, key, value) {
+    let result = null;
+    const filter = JSON.parse(`{\"${key}\":\"${value}\"}`)
+    if (collection === 'users') {
+      result = await this.usersCollection.findOne(filter)
+    } else if (collection === 'files') {
+      result = await this.filesCollection.findOne(filter)
+    }
+    return result
+  }
+
+  async saveUser(user) {
+    const reply = await this.usersCollection.insertOne(user)
+    return reply.ops[0]._id  // id of inserted user
+  }
+
+  async saveFile(file) {
+    const reply = await this.filesCollection.insertOne(file)
+    return reply.ops[0]._id  // id of file
+  }
+
 }
 
 const dbClient = new DBClient();
