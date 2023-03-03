bcbcae7f6f9c04677499cf263795962b9f4f02cc
Iyusuf40 <punkgts40@gmail.com>
2023-03-03 11:49:09 +0100

    add db client

---
+++ utils/db.js
@@ -0,0 +1,39 @@
+import { MongoClient } from 'mongodb';
+
+const host = process.env.DB_HOST || 'localhost';
+const port = process.env.DB_PORT || 27017;
+const dbName = process.env.DB_DATABASE || 'files_manager';
+const url = `mongodb://${host}:${port}`;
+
+class DBClient {
+  constructor() {
+    this.client = MongoClient(url, { useUnifiedTopology: true });
+    this.connect();
+    this.connected = false;
+  }
+
+  async connect() {
+    await this.client.connect();
+    this.connected = true;
+    this.db = this.client.db(dbName);
+  }
+
+  isAlive() {
+    return this.connected;
+  }
+
+  async nbUsers() {
+    const usersCollection = this.db.collection('users');
+    const noOfCollections = await usersCollection.countDocuments({});
+    return noOfCollections;
+  }
+
+  async nbFiles() {
+    const filesCollection = this.db.collection('files');
+    const noOfFiles = await filesCollection.countDocuments({});
+    return noOfFiles;
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
