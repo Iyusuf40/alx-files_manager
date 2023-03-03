746e652b8b7c15e09ca48c01b78a4b9a91e63e19
Iyusuf40 <punkgts40@gmail.com>
2023-03-03 17:26:59 +0100

    add userController

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -9,7 +9,7 @@ export default class UsersController {
       const password = req.body.password
       if (!email) return res.status(400).send({error: "Missing email"})
       if (!password) return res.status(400).send({error: "Missing password"})
-      exists = await dbClient.findByColAndFilter('users', 'email', email)
+      const exists = await dbClient.findByColAndFilter('users', 'email', email)
       if (exists) return res.status(400).send({error: "Already exist"})
       const hashedPwd = UsersController.hashPwd(password)
       const user = { email: email, password: hashedPwd }
